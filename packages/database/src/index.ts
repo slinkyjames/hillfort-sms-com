1419f89a47e50911b4e8fe79537e5c590b4adf72
Augustine James Ocheifa <slinknjame@gmail.com>
2026-08-16 09:57:25 +0000

    fix: pass datasourceUrl to PrismaClient and remove from schema

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -7,6 +7,8 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    // Pass the connection string dynamically here instead of the schema
+    datasourceUrl: process.env.DATABASE_URL, 
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
 
